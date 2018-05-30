dc489834bb013a54300c5ea5f8c0aceed5175360
Tmpod <35771816+Tmpod@users.noreply.github.com>
2018-05-30 19:48:48 +0100

    Added a boot-up success message and made the ready message prettier

--- bot_template.py
+++ bot_template.py
@@ -1,3 +1,4 @@
+__version__
 """Imports"""
 import discord
 from discord.ext import commands
@@ -12,9 +13,9 @@ from discord.ext import commands
 with open('./yourfilecontainingONLYyourbottoken.txt') as fp:
     TOKEN = fp.read().strip()
 
-prefix = "<yourbotprefix>" #It's really useful to define the prefix in a seperate variable instead of just putting it directily in the 'command_prefix' paramter
+prefix = "<your bot prefix>" #It's really useful to define the prefix in a seperate variable instead of just putting it directily in the 'command_prefix' paramter
 
-bot = commands.Bot(description="<yourbotdescription>", command_prefix=prefix)
+bot = commands.Bot(description="<your bot description>", command_prefix=prefix)
 
 """This piece of code loads all the extensions delcared on the 'extensions' varaiable"""
 #extensions = ['module1', 'module2'] #You can have as many extensions as you want
@@ -32,19 +33,21 @@ bot = commands.Bot(description="<yourbotdescription>", command_prefix=prefix)
 """Boot-up code"""
 @bot.event
 async def on_ready():
-    print('\n======================')
-    print("I'm ready!")
-    print('Logged in as')
-    print(bot.user.name)
-    print("ID: " + str(bot.user.id))
-    print(f"Current prefix: {prefix}")
-    print('========================\n')
+    print(f"""\n=======================
+I'm ready!
+Logged in as:
+{bot.user.name}
+ID:{str(bot.user.id)}
+Current prefix: {prefix}
+Python Version: {sys.version_info[0]}
+Discord Version: {discord.version_info}
+========================\n""")
     """Quite self-explanatory"""
     async def change_activities():
         options = ('Minesweeper', 'Pong', 'Tic-tac toe', 'with the Discord API')
         timeout = 60
         watch = discord.Activity(type=discord.ActivityType.watching, name=f"for {prefix}help")
-        stream = discord.Streaming(url="<yourtwitchlink>", name="bits of information")
+        stream = discord.Streaming(url="<your twitch link>", name="bits of information")
         listen = discord.Activity(type=discord.ActivityType.listening, name="to beeps and bops")
         while True:
             game = discord.Game(name=random.choice(options))
@@ -54,6 +57,9 @@ async def on_ready():
 
     # To fire up the worker in the background:
     bot.loop.create_task(change_activities())
+
+    channel = utils.get(guild.channels, name="<the channel name you wanna send a Boot-up success message>")
+    await channel.send(":white_check_mark: {bot.user.name} successfuly booted-up!")
 
 
 bot.run(TOKEN)
