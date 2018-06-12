16cc1e0385f2a1ec3f20bf9f81bbbdf8a158c828
Tmpod <35771816+Tmpod@users.noreply.github.com>
2018-06-12 21:47:42 +0100

    Quite big update

--- bot_template_main.py
+++ bot_template_main.py
@@ -2,31 +2,26 @@ __version__ = '<your bot version>'
 """Imports"""
 import discord
 from discord.ext import commands
-#import logging # This is not necessary
-#import traceback # This is not necessary
+import random # This is needed for the changing of activities
+import asyncio # And this too
+import logging 
+import traceback 
 
 """Setting up loggin"""
-#logging.basicConfig(level='INFO')
-#logger = logging.getLogger('thisfile.py')
+logging.basicConfig(level='INFO')
+logger = logging.getLogger('<thisfile>')
 
 """Getting your bot token from a external file so that you can share your code easily without having to always black it out"""
-with open('YOURBOTBASEDATA.json') as json_data:
-    basedata = json.load(json_data)
-    prefix = basedata['prefix'] # It's really useful to define the prefix in a seperate variable instead of just putting it directily in the 'command_prefix' paramter
+with open('./yourfilecontainingONLYyourbottoken.txt') as fp:
+    TOKEN = fp.read().strip()
 
-
+prefix = "<your bot prefix>" #It's really useful to define the prefix in a seperate variable instead of just putting it directily in the 'command_prefix' paramter
 
 bot = commands.Bot(description="<your bot description>", command_prefix=prefix)
 
-"""This piece of code loads all the extensions delcared on the 'extensions' varaiable"""
-#extensions = ['module1', 'module2'] # You can have as many extensions as you want
+"""Declaring the extensions to load"""
+extensions = ['module1', 'module2'] # You can have as many extensions as you want
 
-#for extension in extensions:
-#    try:
-#        bot.load_extension(extension)
-#    except Exception:
-#        print(globals())
-#        logger.error(f'Failed to load {extension} with error:\n{traceback.format_exc()}')
 
 """Removing the default help command so that you can make a better and fancier one later. Not mandatory."""
 #bot.remove_command('help')
@@ -43,26 +38,38 @@ Creator: {creator}
 Current prefix: {prefix}
 Python Version: {sys.version_info[0]}
 Discord Version: {discord.version_info[0]}
-CyborgToast Version: {__version__}
+{bot.user.name} Version: {__version__}
 ===========================\n""")
     """Quite self-explanatory"""
     async def change_activities():
-        options = ('Minesweeper', 'Pong', 'Tic-tac toe', 'with the Discord API') # All the options for the "Playing <smth>" status
-        timeout = 60  # The time between each change of status
-        watch = discord.Activity(type=discord.ActivityType.watching, name=f"for {prefix}help") # "Watching <smth>" status
-        stream = discord.Streaming(url="<your twitch link>", name="bits of information") # "Streaming <smth>" status
-        listen = discord.Activity(type=discord.ActivityType.listening, name="to beeps and bops") # "Listening <smth>" status
+        timeout = 60  # The time between each change of status in seconds
+        playopt = ['Minesweeper', 'Pong', 'Tic-tac toe', 'with the Discord API'] # All the options for the "Playing <smth>" status
+        watchopt = [f'for {prefix}help'] # All the options for the "Watching <smth>" status
+        streamopt = [' bit of information'] # All the options for the "Streaming <smth>" status
+        listenopt = ['to beeps and bops'] # All the options for the "Listening <smth>" status
         while True: # Infinite loop
-            game = discord.Game(name=random.choice(options)) # Pick a choice from 'options'. Watch out for how you import 'random', as that might affect how this line needs to be written. For more help refer to the Python Docs.
+            game = discord.Game(name=random.choice(playopt)) # Pick a choice from 'playopt'. Watch out for how you import 'random', as that might affect how this line needs to be written. For more help refer to the Python Docs.
+            watch = discord.Activity(type=discord.ActivityType.watching, name=random.choice(watchopt)) # Pick a choice from 'watchopt'
+            stream = discord.Streaming(url="<your twitch link>", name=random.choice(streamopt)) # Pick a choice from 'streamopt'
+            listen = discord.Activity(type=discord.ActivityType.listening, name=random.choice(listenopt)) # Pick a choice from 'listenopt'
             possb = random.choice([watch, stream, game, listen]) # Pick a choice from all the possibilities: "Playing", "Watching", "Streaming", "Listening" or "Playing"
             await bot.change_presence(activity=possb) # Now change the status to the chosen pick
             await asyncio.sleep(timeout) # And wait for 'timeout' seconds
 
     bot.loop.create_task(change_activities())  # To fire up the worker in the background:
 
-    channel = bot.get_channel('<channel id in a int>') # Gets a discord.TextChannel from the id you insert. YOU MUST INSERT A INT, I've put a string because of syntax highlighting
-    em = discord.Embed(description=f":white_check_mark: **{bot.user.name} successfuly booted-up!**", color=discord.Colour.green()) # Creating an pretty embed 
-    await channel.send(embed=em) # And sends a message there
+    """This piece of code loads all the extensions delcared on the 'extensions' varaiable"""
+    for extension in extensions:
+       try:
+           bot.load_extension(extension)
+       except Exception:
+           print(globals())
+           logger.error(f'\nFailed to load {extension} with error:\n{traceback.format_exc()}')
+           
+    """This part will send a message to the given channel (option 1) or a DM to you (option 2)"""
+    channel = bot.get_channel('<channel id in a int>') # Gets a discord.TextChannel from the id you insert. YOU MUST INSERT A INT, I've put a string because of syntax highlighting ### OPTION 1
+    await channel.send(embed=discord.Embed(description=f":white_check_mark: **{bot.user.name} successfuly booted-up!**", color=discord.Colour.green()) # And sends a pretty embed there ### OPTION 1
+    await creator.send(embed=discord.Embed(description=f":white_check_mark: **{bot.user.name} successfuly booted-up!**", color=discord.Colour.green())) ### OPTION 2
 
 
 
